ee061e903a8f61384394c61110576f523fe95854
Connor Genyk <genyk-c@webmail.uwinnipeg.ca>
2019-04-06 17:41:26 -0500

    Added Boarder and changed shape
    Signed-off-by: Connor Genyk <genyk-c@webmail.uwinnipeg.ca>

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import simpleguitk as simplegui
-#commit
+
+# commit
 x = 50
 y = 200
 
@@ -9,21 +10,30 @@ def draw_handler(canvas):
     global y
     color = "RGB(15, 150, 150)"
     x = x + 20
+    x2 = x + 20
 
     if (x >= 1000):
         x = 50
+    y = 200
+    ## Loading Dots
+    canvas.draw_circle((x, y), 5, 1, color, color)
+    canvas.draw_circle((x2, y), 5, 1, color, color)
+    canvas.draw_circle((x2 + 20, y), 5, 1, color, color)
+    canvas.draw_circle((x2 + 20, y), 5, 1, color, color)
+    canvas.draw_circle((x2 + 40, y), 5, 1, color, color)
+    canvas.draw_circle((x2 + 60, y), 5, 1, color, color)
+    canvas.draw_circle((x2 + 80, y), 5, 1, color, color)
 
-    canvas.draw_circle((x, y), 50, 1, color, color)
-    canvas.draw_circle((550, 120), 20, 12, 'Green')  # Green Boi
-    canvas.draw_circle([432, 220], 30, 12, 'Red')  # Red Boi
-    canvas.draw_circle((342, 320), 20, 5, 'Blue', 'White')  # Blue White Boi
-    canvas.draw_circle([120, 80], 30, 10, 'Yellow', 'Orange')
-    # TODO Add 3 more polygons
-    canvas.draw_polygon([[150, 180], [160, 190], [40, 20], [10, 10]], 12, 'Red', 'Red')
-    canvas.draw_polygon([(90, 100), (80, 40), (30, 90)], 5, 'Blue', 'White')
-    # Todo add 3 more lines
-    canvas.draw_line([500, 70], [610, 70], 5, 'Blue')
-    frame.set_canvas_background('Black')
+    # TODO Add 4 more polygons
+    canvas.draw_polygon([[0, 0], [1000, 0], [1000, 400], [0, 400]], 12, 'Black')
+
+    # Todo add 4 more lines
+    canvas.draw_line([420, 175], [580, 175], 5, 'Black')
+
+    # Add Text
+    canvas.draw_text('Loading', (420, 160), 48, 'Black', 'serif')
+
+    frame.set_canvas_background('White')
 
 
 for x in (0, 100):
@@ -32,4 +42,5 @@ for x in (0, 100):
 frame = simplegui.create_frame("Go", 1000, 400)
 frame.set_draw_handler(draw_handler)
 frame.start()
+
 
